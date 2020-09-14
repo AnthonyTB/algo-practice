831be8d17ec26363515eecc230963a2ab67f66af
AnthonyTB <anthonylol@protonmail.com>
2020-09-13 20:08:35 -0700

    intro - completed 2.1 plus added prettier config

--- intro-algo.js
+++ intro-algo.js
@@ -1,14 +1,25 @@
 //input: [1, 5, 2, 1] -> output: [1, 2, 5]
 //input: [4, 2, 2, 3, 2, 2, 2] -> output: [2, 3, 4]
 var uniqSort = function (arr) {
-    var breadcrumbs = {};
-    var result = [arr[0]];
-    for (var i = 1; i < arr.length; i++) {
-        if (!breadcrumbs[arr[i]]) {
+    var cache = {};
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (!cache[arr[i]]) {
             result.push(arr[i]);
-            breadcrumbs[arr[i]] = true;
+            cache[arr[i]] = true;
         }
     }
     return result.sort(function (a, b) { return a - b; });
 };
 console.log(uniqSort([4, 2, 2, 3, 2, 2, 2]));
+// ---------------------------------------------------------------
+var cache = {};
+var memoTimes10 = function (input) {
+    if (input in cache) {
+        return cache[input];
+    }
+    else {
+        cache[input] = input * 10;
+        return input * 10;
+    }
+};
--- intro-algo.ts
+++ intro-algo.ts
@@ -1,19 +1,31 @@
-
 //input: [1, 5, 2, 1] -> output: [1, 2, 5]
 //input: [4, 2, 2, 3, 2, 2, 2] -> output: [2, 3, 4]
 
 const uniqSort = (arr: number[]): number[] => {
-const breadcrumbs = {};
-const result = [arr[0]];
+  const cache = {};
+  const result = [];
 
-for (let i = 1; i < arr.length; i++) {
-if (!breadcrumbs[arr[i]]) {
-    result.push(arr[i]);
-    breadcrumbs[arr[i]] = true;
-}
-}
+  for (let i = 0; i < arr.length; i++) {
+    if (!cache[arr[i]]) {
+      result.push(arr[i]);
+      cache[arr[i]] = true;
+    }
+  }
 
-return result.sort((a, b) => a - b);
-}
+  return result.sort((a, b) => a - b);
+};
 
 console.log(uniqSort([4, 2, 2, 3, 2, 2, 2]));
+
+// ---------------------------------------------------------------
+
+const cache = {};
+
+const memoTimes10 = (input: number): number => {
+  if (input in cache) {
+    return cache[input];
+  } else {
+    cache[input] = input * 10;
+    return input * 10;
+  }
+};
